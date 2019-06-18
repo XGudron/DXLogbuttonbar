40d9a3c45c6db75f398b291e578f17b9de19da86
Björn Ekelund <bjorn@ekelund.nu>
2019-06-18 22:40:52 +0200

    Add project files.

---
+++ DXLCusForm1/CusFrmTest1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DXLog.net
+{
+    partial class CusFrmTest1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.Location = new System.Drawing.Point(12, 9);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(259, 244);
+            this.lbInfo.TabIndex = 1;
+            // 
+            // CusFrmTest1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lbInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormID = 1000;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CusFrmTest1";
+            this.Text = "My custom form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbInfo;
+    }
+}
