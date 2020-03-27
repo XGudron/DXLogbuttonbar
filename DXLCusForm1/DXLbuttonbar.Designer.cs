249f0b376a349331e56a2c1e8b778304ab67fcb9
Björn Ekelund <bjorn@ekelund.nu>
2020-03-27 16:25:33 +0100

    Added 12 buttons for Alt messages

--- DXLCusForm1/DXLbuttonbar.Designer.cs
+++ DXLCusForm1/DXLbuttonbar.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbInfo = new System.Windows.Forms.Label();
             this.F1 = new System.Windows.Forms.Button();
             this.F2 = new System.Windows.Forms.Button();
             this.F3 = new System.Windows.Forms.Button();
@@ -37,21 +36,27 @@
             this.F6 = new System.Windows.Forms.Button();
             this.F7 = new System.Windows.Forms.Button();
             this.Ins = new System.Windows.Forms.Button();
+            this.PLus = new System.Windows.Forms.Button();
+            this.AltF1 = new System.Windows.Forms.Button();
+            this.AltF2 = new System.Windows.Forms.Button();
+            this.AltF3 = new System.Windows.Forms.Button();
+            this.AltF4 = new System.Windows.Forms.Button();
+            this.AltF5 = new System.Windows.Forms.Button();
+            this.AltF10 = new System.Windows.Forms.Button();
+            this.AltF9 = new System.Windows.Forms.Button();
+            this.AltF8 = new System.Windows.Forms.Button();
+            this.AltF7 = new System.Windows.Forms.Button();
+            this.AltF6 = new System.Windows.Forms.Button();
+            this.AltF11 = new System.Windows.Forms.Button();
+            this.AltF12 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lbInfo
-            // 
-            this.lbInfo.Location = new System.Drawing.Point(12, 9);
-            this.lbInfo.Name = "lbInfo";
-            this.lbInfo.Size = new System.Drawing.Size(259, 244);
-            this.lbInfo.TabIndex = 1;
             // 
             // F1
             // 
             this.F1.BackColor = System.Drawing.SystemColors.Highlight;
             this.F1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.F1.ForeColor = System.Drawing.Color.White;
-            this.F1.Location = new System.Drawing.Point(15, 9);
+            this.F1.Location = new System.Drawing.Point(3, 4);
             this.F1.Name = "F1";
             this.F1.Size = new System.Drawing.Size(75, 23);
             this.F1.TabIndex = 2;
@@ -64,7 +69,7 @@
             this.F2.BackColor = System.Drawing.SystemColors.Highlight;
             this.F2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.F2.ForeColor = System.Drawing.Color.White;
-            this.F2.Location = new System.Drawing.Point(96, 9);
+            this.F2.Location = new System.Drawing.Point(84, 4);
             this.F2.Name = "F2";
             this.F2.Size = new System.Drawing.Size(75, 23);
             this.F2.TabIndex = 3;
@@ -77,7 +82,7 @@
             this.F3.BackColor = System.Drawing.SystemColors.Highlight;
             this.F3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.F3.ForeColor = System.Drawing.Color.White;
-            this.F3.Location = new System.Drawing.Point(177, 9);
+            this.F3.Location = new System.Drawing.Point(165, 4);
             this.F3.Name = "F3";
             this.F3.Size = new System.Drawing.Size(75, 23);
             this.F3.TabIndex = 4;
@@ -90,7 +95,7 @@
             this.F4.BackColor = System.Drawing.SystemColors.Highlight;
             this.F4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.F4.ForeColor = System.Drawing.Color.White;
-            this.F4.Location = new System.Drawing.Point(258, 9);
+            this.F4.Location = new System.Drawing.Point(246, 4);
             this.F4.Name = "F4";
             this.F4.Size = new System.Drawing.Size(75, 23);
             this.F4.TabIndex = 5;
@@ -103,7 +108,7 @@
             this.F5.BackColor = System.Drawing.SystemColors.Highlight;
             this.F5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.F5.ForeColor = System.Drawing.Color.White;
-            this.F5.Location = new System.Drawing.Point(15, 38);
+            this.F5.Location = new System.Drawing.Point(3, 33);
             this.F5.Name = "F5";
             this.F5.Size = new System.Drawing.Size(75, 23);
             this.F5.TabIndex = 6;
@@ -116,7 +121,7 @@
             this.F6.BackColor = System.Drawing.SystemColors.Highlight;
             this.F6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.F6.ForeColor = System.Drawing.Color.White;
-            this.F6.Location = new System.Drawing.Point(96, 38);
+            this.F6.Location = new System.Drawing.Point(84, 33);
             this.F6.Name = "F6";
             this.F6.Size = new System.Drawing.Size(75, 23);
             this.F6.TabIndex = 7;
@@ -129,7 +134,7 @@
             this.F7.BackColor = System.Drawing.SystemColors.Highlight;
             this.F7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.F7.ForeColor = System.Drawing.Color.White;
-            this.F7.Location = new System.Drawing.Point(177, 38);
+            this.F7.Location = new System.Drawing.Point(165, 33);
             this.F7.Name = "F7";
             this.F7.Size = new System.Drawing.Size(75, 23);
             this.F7.TabIndex = 8;
@@ -142,7 +147,7 @@
             this.Ins.BackColor = System.Drawing.SystemColors.Highlight;
             this.Ins.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Ins.ForeColor = System.Drawing.Color.White;
-            this.Ins.Location = new System.Drawing.Point(258, 38);
+            this.Ins.Location = new System.Drawing.Point(327, 4);
             this.Ins.Name = "Ins";
             this.Ins.Size = new System.Drawing.Size(75, 23);
             this.Ins.TabIndex = 9;
@@ -150,11 +155,193 @@
             this.Ins.UseVisualStyleBackColor = false;
             this.Ins.Click += new System.EventHandler(this.Ins_Click);
             // 
+            // PLus
+            // 
+            this.PLus.BackColor = System.Drawing.SystemColors.Highlight;
+            this.PLus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PLus.ForeColor = System.Drawing.Color.White;
+            this.PLus.Location = new System.Drawing.Point(408, 4);
+            this.PLus.Name = "PLus";
+            this.PLus.Size = new System.Drawing.Size(75, 23);
+            this.PLus.TabIndex = 10;
+            this.PLus.Text = "Plus";
+            this.PLus.UseVisualStyleBackColor = false;
+            this.PLus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // AltF1
+            // 
+            this.AltF1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF1.ForeColor = System.Drawing.Color.White;
+            this.AltF1.Location = new System.Drawing.Point(3, 62);
+            this.AltF1.Name = "AltF1";
+            this.AltF1.Size = new System.Drawing.Size(75, 23);
+            this.AltF1.TabIndex = 11;
+            this.AltF1.Text = "Alt Msg 1";
+            this.AltF1.UseVisualStyleBackColor = false;
+            this.AltF1.Click += new System.EventHandler(this.AltF1_Click);
+            // 
+            // AltF2
+            // 
+            this.AltF2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF2.ForeColor = System.Drawing.Color.White;
+            this.AltF2.Location = new System.Drawing.Point(84, 62);
+            this.AltF2.Name = "AltF2";
+            this.AltF2.Size = new System.Drawing.Size(75, 23);
+            this.AltF2.TabIndex = 12;
+            this.AltF2.Text = "Alt Msg 2";
+            this.AltF2.UseVisualStyleBackColor = false;
+            this.AltF2.Click += new System.EventHandler(this.AltF2_Click);
+            // 
+            // AltF3
+            // 
+            this.AltF3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF3.ForeColor = System.Drawing.Color.White;
+            this.AltF3.Location = new System.Drawing.Point(165, 62);
+            this.AltF3.Name = "AltF3";
+            this.AltF3.Size = new System.Drawing.Size(75, 23);
+            this.AltF3.TabIndex = 13;
+            this.AltF3.Text = "Alt Msg 3";
+            this.AltF3.UseVisualStyleBackColor = false;
+            this.AltF3.Click += new System.EventHandler(this.AltF3_Click);
+            // 
+            // AltF4
+            // 
+            this.AltF4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF4.ForeColor = System.Drawing.Color.White;
+            this.AltF4.Location = new System.Drawing.Point(246, 62);
+            this.AltF4.Name = "AltF4";
+            this.AltF4.Size = new System.Drawing.Size(75, 23);
+            this.AltF4.TabIndex = 14;
+            this.AltF4.Text = "Alt Msg 4";
+            this.AltF4.UseVisualStyleBackColor = false;
+            this.AltF4.Click += new System.EventHandler(this.AltF4_Click);
+            // 
+            // AltF5
+            // 
+            this.AltF5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF5.ForeColor = System.Drawing.Color.White;
+            this.AltF5.Location = new System.Drawing.Point(327, 62);
+            this.AltF5.Name = "AltF5";
+            this.AltF5.Size = new System.Drawing.Size(75, 23);
+            this.AltF5.TabIndex = 15;
+            this.AltF5.Text = "Alt Msg 5";
+            this.AltF5.UseVisualStyleBackColor = false;
+            this.AltF5.Click += new System.EventHandler(this.AltF5_Click);
+            // 
+            // AltF10
+            // 
+            this.AltF10.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF10.ForeColor = System.Drawing.Color.White;
+            this.AltF10.Location = new System.Drawing.Point(246, 91);
+            this.AltF10.Name = "AltF10";
+            this.AltF10.Size = new System.Drawing.Size(75, 23);
+            this.AltF10.TabIndex = 20;
+            this.AltF10.Text = "Alt Msg 10";
+            this.AltF10.UseVisualStyleBackColor = false;
+            this.AltF10.Click += new System.EventHandler(this.AltF10_Click);
+            // 
+            // AltF9
+            // 
+            this.AltF9.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF9.ForeColor = System.Drawing.Color.White;
+            this.AltF9.Location = new System.Drawing.Point(165, 91);
+            this.AltF9.Name = "AltF9";
+            this.AltF9.Size = new System.Drawing.Size(75, 23);
+            this.AltF9.TabIndex = 19;
+            this.AltF9.Text = "Alt Msg 9";
+            this.AltF9.UseVisualStyleBackColor = false;
+            this.AltF9.Click += new System.EventHandler(this.AltF9_Click);
+            // 
+            // AltF8
+            // 
+            this.AltF8.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF8.ForeColor = System.Drawing.Color.White;
+            this.AltF8.Location = new System.Drawing.Point(84, 91);
+            this.AltF8.Name = "AltF8";
+            this.AltF8.Size = new System.Drawing.Size(75, 23);
+            this.AltF8.TabIndex = 18;
+            this.AltF8.Text = "Alt Msg 8";
+            this.AltF8.UseVisualStyleBackColor = false;
+            this.AltF8.Click += new System.EventHandler(this.AltF8_Click);
+            // 
+            // AltF7
+            // 
+            this.AltF7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF7.ForeColor = System.Drawing.Color.White;
+            this.AltF7.Location = new System.Drawing.Point(3, 91);
+            this.AltF7.Name = "AltF7";
+            this.AltF7.Size = new System.Drawing.Size(75, 23);
+            this.AltF7.TabIndex = 17;
+            this.AltF7.Text = "Alt Msg 7";
+            this.AltF7.UseVisualStyleBackColor = false;
+            this.AltF7.Click += new System.EventHandler(this.AltF7_Click);
+            // 
+            // AltF6
+            // 
+            this.AltF6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF6.ForeColor = System.Drawing.Color.White;
+            this.AltF6.Location = new System.Drawing.Point(408, 62);
+            this.AltF6.Name = "AltF6";
+            this.AltF6.Size = new System.Drawing.Size(75, 23);
+            this.AltF6.TabIndex = 16;
+            this.AltF6.Text = "Alt Msg 6";
+            this.AltF6.UseVisualStyleBackColor = false;
+            this.AltF6.Click += new System.EventHandler(this.AltF6_Click);
+            // 
+            // AltF11
+            // 
+            this.AltF11.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF11.ForeColor = System.Drawing.Color.White;
+            this.AltF11.Location = new System.Drawing.Point(327, 91);
+            this.AltF11.Name = "AltF11";
+            this.AltF11.Size = new System.Drawing.Size(75, 23);
+            this.AltF11.TabIndex = 21;
+            this.AltF11.Text = "Alt Msg 11";
+            this.AltF11.UseVisualStyleBackColor = false;
+            this.AltF11.Click += new System.EventHandler(this.AltF11_Click);
+            // 
+            // AltF12
+            // 
+            this.AltF12.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AltF12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AltF12.ForeColor = System.Drawing.Color.White;
+            this.AltF12.Location = new System.Drawing.Point(408, 91);
+            this.AltF12.Name = "AltF12";
+            this.AltF12.Size = new System.Drawing.Size(75, 23);
+            this.AltF12.TabIndex = 22;
+            this.AltF12.Text = "Alt Msg 12";
+            this.AltF12.UseVisualStyleBackColor = false;
+            this.AltF12.Click += new System.EventHandler(this.AltF12_Click);
+            // 
             // DXLbuttonbar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 70);
+            this.ClientSize = new System.Drawing.Size(496, 130);
+            this.Controls.Add(this.AltF12);
+            this.Controls.Add(this.AltF11);
+            this.Controls.Add(this.AltF10);
+            this.Controls.Add(this.AltF9);
+            this.Controls.Add(this.AltF8);
+            this.Controls.Add(this.AltF7);
+            this.Controls.Add(this.AltF6);
+            this.Controls.Add(this.AltF5);
+            this.Controls.Add(this.AltF4);
+            this.Controls.Add(this.AltF3);
+            this.Controls.Add(this.AltF2);
+            this.Controls.Add(this.AltF1);
+            this.Controls.Add(this.PLus);
             this.Controls.Add(this.Ins);
             this.Controls.Add(this.F7);
             this.Controls.Add(this.F6);
@@ -163,20 +350,17 @@
             this.Controls.Add(this.F3);
             this.Controls.Add(this.F2);
             this.Controls.Add(this.F1);
-            this.Controls.Add(this.lbInfo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.FormID = 1004;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DXLbuttonbar";
-            this.Text = "CW button bar";
+            this.Text = "Message button bar";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lbInfo;
         private System.Windows.Forms.Button F1;
         private System.Windows.Forms.Button F2;
         private System.Windows.Forms.Button F3;
@@ -185,5 +369,18 @@
         private System.Windows.Forms.Button F6;
         private System.Windows.Forms.Button F7;
         private System.Windows.Forms.Button Ins;
+        private System.Windows.Forms.Button PLus;
+        private System.Windows.Forms.Button AltF1;
+        private System.Windows.Forms.Button AltF2;
+        private System.Windows.Forms.Button AltF3;
+        private System.Windows.Forms.Button AltF4;
+        private System.Windows.Forms.Button AltF5;
+        private System.Windows.Forms.Button AltF10;
+        private System.Windows.Forms.Button AltF9;
+        private System.Windows.Forms.Button AltF8;
+        private System.Windows.Forms.Button AltF7;
+        private System.Windows.Forms.Button AltF6;
+        private System.Windows.Forms.Button AltF11;
+        private System.Windows.Forms.Button AltF12;
     }
 }
--- DXLCusForm1/DXLbuttonbar.cs
+++ DXLCusForm1/DXLbuttonbar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace DXLog.net
 {
@@ -7,7 +8,7 @@ namespace DXLog.net
     {
         public static string CusWinName
         {
-            get { return "CW button bar"; }
+            get { return "Message button bar"; }
         }
 
         public static int CusFormID
@@ -49,12 +50,15 @@ namespace DXLog.net
 
             if (mainForm == null)
             {
-                mainForm = (FrmMain)(ParentForm == null ? Owner : ParentForm);
+                mainForm = (FrmMain)(ParentForm ?? Owner);
             }
+
+            contextMenuStrip1.Items.RemoveByKey("fixWindowSizeToolStripMenuItem");
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
         }
 
         // Valid arguments for SendKeyMessage are F1-F7, INSERT, PLUS, and ALTF1-F12.
-        
+
         private void F1_Click(object sender, EventArgs e)
         {
             mainForm.SendKeyMessage(false, "F1");
@@ -93,6 +97,71 @@ namespace DXLog.net
         private void Ins_Click(object sender, EventArgs e)
         {
             mainForm.SendKeyMessage(false, "INSERT");
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "PLUS");
+        }
+
+        private void AltF1_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF1");
+        }
+
+        private void AltF2_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF2");
+        }
+
+        private void AltF3_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF3");
+        }
+
+        private void AltF4_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF4");
+        }
+
+        private void AltF5_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF5");
+        }
+
+        private void AltF6_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF6");
+        }
+
+        private void AltF7_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF7");
+        }
+
+        private void AltF8_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF8");
+        }
+
+        private void AltF9_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF9");
+        }
+
+        private void AltF10_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF10");
+        }
+
+        private void AltF11_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF11");
+        }
+
+        private void AltF12_Click(object sender, EventArgs e)
+        {
+            mainForm.SendKeyMessage(false, "ALTF12");
         }
     }
 }
